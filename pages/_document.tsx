5c925ca4c1114271da7c6ba804801ef0ea9cc088
SeongMin Han <hsm950825@naver.com>
2021-01-19 23:30:08 +0900

    Update: Typescript 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -44,3 +50,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
