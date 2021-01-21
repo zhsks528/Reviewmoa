39cc49c1148a23aca41f1a9ef1bebf22db346cdc
SeongMin Han <hsm950825@naver.com>
2021-01-21 22:33:03 +0900

    Add : 리뷰 삭제 페이지 생성

--- pages/review/delete.tsx
+++ pages/review/delete.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 interface Props {}
 
 const Delete: React.FC<Props> = () => {
-  return <div>삭제 페이지</div>;
+  return (
+    <div>
+      <Link href="/review">
+        <button>글 목록</button>
+      </Link>
+      <div>삭제 페이지</div>;
+    </div>
+  );
 };
 
 export default Delete;
