20a5828d5b4eb2c8049dc775913c51ab402a78ae
SeongMin Han <hsm950825@naver.com>
2021-01-28 02:16:39 +0900

    Add: 제품(products) 모델 추가

---
+++ server/model/products.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    stars: { type: Number },
+    reviews: [{ type: Schema.Types.ObjectId, ref: "review" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+productSchema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+module.exports = mongoose.model("product", productSchema);
